d713ec31799541e9c928322f83711ef6007afaa4
andrewibrah <157662519+andrewibrah@users.noreply.github.com>
2026-03-31 12:57:35 -0400

    Add Eula route to App component
    Signed-off-by: andrewibrah <157662519+andrewibrah@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Privacy from "./pages/Privacy";
 import Support from "./pages/Support";
-
+import Eula from "./pages/Eula";
 export default function App() {
   return (
     <div className="flex min-h-screen flex-col">
@@ -14,6 +14,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/support" element={<Support />} />
+           <Route path="/eula" element={<Eula />} />
         </Routes>
       </main>
       <Footer />
